0e8527de470f7f631c0a3c5fb1f6193c549f2f41
NishiGaba <gabanishi45@gmail.com>
2021-01-07 23:51:10 +0530

    Fetch Random Users with Spinner

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'randomUser'`, () => {
+  it(`should have as title 'random-card'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('randomUser');
+    expect(app.title).toEqual('random-card');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to randomUser!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to random-card!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "./services/user.service";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'randomUser';
+export class AppComponent implements OnInit {
+  title = "random-user";
+  user: any = null;
+  loadingBlock = false;
+  requests = 0; //Number of API requests
+
+  constructor(
+    private userService: UserService,
+    private toastr: ToastrService,
+  ) {}
+
+  ngOnInit() {
+  }
+
+  fetchUser() {
+    this.userService.getUser().subscribe(
+      (user: any) => {
+        //Decrementing the number of requests by 1 after getting response from API
+        this.requests = this.requests - 1;
+        if(this.requests == 0) {
+          this.user = user.results[0]; 
+          this.loadingBlock = false;
+        }    
+      },
+      (err) => {
+        //Decrementing the number of requests by 1 after getting response from API
+        this.requests = this.requests - 1;
+        if(this.requests == 0) {
+          this.toastr.error(err.status, "Oopps,Try Again");
+          this.loadingBlock = false;
+        }   
+      },
+    );
+  }
+
+  handleClick() {
+    this.user = null;
+    this.loadingBlock = true;
+    //Incrementing the number of requests by 1 on user click
+    this.requests = this.requests + 1;
+    let angularThis = this;
+
+    setTimeout(function(){
+      angularThis.fetchUser();
+    }, 2000);
+  }
 }
